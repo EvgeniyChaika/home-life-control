0e7a1b7afe4d60667d5ea8bd9d06681c370cc995
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2017-01-16 16:29:42 +0200

    Added Webpack config

--- server/src/java/com/control/life/home/controllers/MainController.java
+++ server/src/java/com/control/life/home/controllers/MainController.java
@@ -1,8 +1,14 @@
 package com.control.life.home.controllers;
 
-/**
- * Created by evgeniy on 14.01.17.
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class MainController {
 
+    @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
+    public String indexPage() {
+        return "index";
+    }
 }
