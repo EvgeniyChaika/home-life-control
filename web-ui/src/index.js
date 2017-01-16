687d66e883f2dc0bb29d4322b9547fbb1f323c66
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2017-01-16 16:45:18 +0200

    Init commit

---
+++ web-ui/src/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('index');
+
+import './styles/style.css';
+
+import  'jquery';
+
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+import uiBootstrap from 'angular-ui-bootstrap';
+import uiNotification from 'angular-ui-notification';
